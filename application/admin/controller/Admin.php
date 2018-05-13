4fe3bdd29e3a761d847ab11a16acdbf2490bdb95
SnowmanNunu <345750542@qq.com>
2018-05-13 17:02:46 +0800

    add admin

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -17,6 +17,18 @@ class Admin extends Controller
 
     public function add()
     {
+    	if (request()->isPost()) {
+    		$data = input('post.');
+    		$res = db('bk_admin')->insert($data);
+
+    		if($res){
+    			$this->success('添加管理员成功！',url('lst'));
+    		}else{
+    			$this->error('添加失败！');
+    		}
+    		return;
+    	}
+
     	return $this->fetch();
     }
 
