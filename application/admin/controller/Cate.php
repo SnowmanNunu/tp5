0d217a24801cf4af86ebd545bce8043654120ea5
SnowmanNunu <345750542@qq.com>
2018-05-18 10:37:13 +0800

    cate

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -18,7 +18,22 @@ class cate extends Common
 
     public function add()
     {
+       $cate = new CateModel();
+
+      if (request()->isPost()) {
+        //$data = input('post.');
+        $cate->data(input('post.'));
+        $add = $cate->save();
+        if ($add) {
+          $this->success('添加栏目成功!',url('lst'));
+        }else{
+          $this->error('添加栏目失败!');
+        }
+      }
       
+      $cateres=$cate->select();
+      $this->assign('cateres',$cateres);
+      return $this->fetch();
     }
 
 
