17593570c31ae0fd7b7fa95329fe88a8cee02f5f
SnowmanNunu <345750542@qq.com>
2018-05-29 18:25:47 +0800

    Auth rule

---
+++ application/admin/controller/AuthRule.php
@@ -0,0 +1,42 @@
+<?php
+namespace app\admin\controller;
+use \think\Controller;
+use app\admin\model\AuthRule as AuthRuleModel;
+use app\admin\controller\Common;
+
+class AuthRule extends Common
+{
+
+  public function lst(){
+
+    $authRuleRes=AuthRuleModel::paginate(5);
+    $this->assign('authRuleRes',$authRuleRes);
+    return view();
+  }    
+
+
+  public function add(){
+    if (request()->isPost()) {
+      $data=input('post.');
+      $plevel=db('auth_rule')->where('id',$data['pid'])->field('level')->find();
+      if ($plevel) {
+        $data['level']=$plevel['level']+1;
+      }else{
+        $data['level']=0;
+      }
+      $add=db('auth_rule')->insert($data);
+      if ($add) {
+        $this->success('添加权限成功!',url('lst'));
+      }else{
+        $this->error('添加权限失败!');
+      }
+      return;
+    }
+    $authRuleRes=db('auth_rule')->select();
+    $this->assign('authRuleRes',$authRuleRes);
+    return view();
+  }
+
+
+
+}
