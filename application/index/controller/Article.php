109feebc246dce5d395f6536d8c33d77ddcbf485
SnowmanNunu <345750542@qq.com>
2018-09-12 14:15:52 +0800

    Login

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -1,11 +1,21 @@
 <?php
 namespace app\index\controller;
-use \think\Controller;
-
+use think\Request;
 class Article extends Common
 {
     public function index()
     {
-        return $this->fetch('article');
+    	$artid=input('artid');
+    	db('article')->where(array('id'=>$artid))->setInc('click');
+    	$articles=db('article')->find($artid);
+    	$article= new \app\index\model\Article();
+    	$hotRes=$article->getHotRes($articles['cateid']);
+    	$this->assign(array(
+    		'articles'=>$articles,
+    		'hotRes'=>$hotRes,
+            'artid'=>$artid,
+    		));
+        return view('article');
     }
+
 }
